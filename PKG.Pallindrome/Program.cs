669c5ac81e8c987fdc5f369d5c0a88b38aff96fa
Maersk Test <goriya.piyush@gmail.com>
2020-05-30 23:55:53 +0530

    Add project files.

---
+++ PKG.Pallindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKG.Pallindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            var line1 = System.Console.ReadLine().Trim();
+            var N = Int32.Parse(line1);
+            for (var i = 0; i < N; i++)
+            {
+                str = System.Console.ReadLine().Trim();
+                string str1 = new string(str.ToCharArray().Reverse().ToArray());
+                if (str1 == str)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
